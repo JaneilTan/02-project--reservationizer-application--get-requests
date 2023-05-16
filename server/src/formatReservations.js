71e60271569d8f03581b464e84c7535f396f5249
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-17 09:57:05 +1200

    Created a formatReservations to replace _id to id

---
+++ server/src/formatReservations.js
@@ -0,0 +1,10 @@
+const formatReservations = (reservations) => {
+    return {
+      id: reservations._id,
+      partySize: reservations.partySize,
+      date: reservations.date,
+      userId: reservations.userId,
+      restaurantName: reservations.restaurantName,
+    };
+};
+  module.exports = formatReservations;

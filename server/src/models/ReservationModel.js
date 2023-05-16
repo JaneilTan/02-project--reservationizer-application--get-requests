eeb5f64c1deae8f9b219053fca68e6ea89ce34be
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-17 09:52:57 +1200

    Created a schema

--- server/src/models/ReservationModel.js
+++ server/src/models/ReservationModel.js
@@ -1,1 +1,17 @@
 // FIXME export a model for Reservations
+const mongoose = require ("mongoose");
+const { Schema } = mongoose;
+
+const reservationSchema = Schema({ 
+  partySize: {type: Number, required: true},
+  date: { type: Date, default: Date.now, required:true },
+  userId: {type: String, required: true},
+  restaurantName: {type: String, required: true},
+ 
+});
+
+
+
+const ReservationModel = mongoose.model("Reservation", reservationSchema);
+
+module.exports = ReservationModel;

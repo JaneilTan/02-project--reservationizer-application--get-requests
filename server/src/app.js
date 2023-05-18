70cf560983e6cf6379ab3778204d345c5e7ec0ed
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-18 14:49:59 +1200

    Undo unneccessary code

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,7 @@ app.get("/reservations/:id", async (req, res) => {
     const id = req.params.id;
 
     if (!validId(id)){
-       return res.sendStatus(400)
+       return res.sendStatus(400) 
     }
 
     const reservation = await ReservationModel.findById(id);
